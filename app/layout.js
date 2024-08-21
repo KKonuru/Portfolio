537b2a5da674f5ed51567c936473190a3ed2b51f
KKonuru <kausthubhkonuru@gmail.com>
2024-08-21 09:48:48 -0400

    updated topnav

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import {TopNav} from './_components/TopNav';
+import {TopNav} from './_components/topnav';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
